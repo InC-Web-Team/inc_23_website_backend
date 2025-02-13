4e60f63c8741b722261a90f87730821fea53e170
piyush960 <piyushdahake096@gmail.com>
2025-02-13 19:31:56 +0530

    fix array extract

--- services/database/admin/admin.database.services.mjs
+++ services/database/admin/admin.database.services.mjs
@@ -4,7 +4,7 @@ import { AppError } from "../../../utils/index.js";
 function adminServices(db) {
   async function findAdmin(username) {
     try {
-      const [results] = await db.execute(adminQueries.findAdmin, [username]).catch(err => { throw new AppError(400, 'fail', err.sqlMessage) })
+      const [[results]] = await db.execute(adminQueries.findAdmin, [username]).catch(err => { throw new AppError(400, 'fail', err.sqlMessage) })
       return results[0]
     } catch (err) {
       throw new AppError(500, 'fail', err)
